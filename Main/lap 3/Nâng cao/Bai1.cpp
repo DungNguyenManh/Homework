a8d00ce11e47fb283c35c537958fcb712b8bca83
DungNguyenManh <dung903698954@gmail.com>
2021-12-15 14:57:15 +0700

    Chane

--- Main/lap 3/Nâng cao/Bai1.cpp
+++ Main/lap 3/Nâng cao/Bai1.cpp
@@ -1,15 +1,36 @@
-#include <math.h>
 #include <stdio.h>
+
 int main()
 {
-    int ngay, thang, nam, songay;
-    printf("nhap ngay:");
-    scanf("%d", &ngay);
-    printf("nhap thang:");
-    scanf("%d", &thang);
-    printf("nhap nam:");
-    scanf("%d", &nam);
-    switch (thang)
+    int day, month, year, songay_trongthang;
+    do
+    {
+        printf("Nhap ngay: ");
+        scanf("%d", &day);
+        if (day <= 0)
+        {
+            printf("Khong hop le. Nhap lai! \n");
+        }
+    } while (day <= 0);
+    do
+    {
+        printf("Nhap thang: ");
+        scanf("%d", &month);
+        if (month <= 0)
+        {
+            printf("Khong hop le. Nhap lai! \n");
+        }
+    } while (month <= 0);
+    do
+    {
+        printf("Nhap nam: ");
+        scanf("%d", &year);
+        if (year <= 0)
+        {
+            printf("Khong hop le. Nhap lai! \n");
+        }
+    } while (year <= 0);
+    switch (month)
     {
     case 1:
     case 3:
@@ -18,25 +39,40 @@ int main()
     case 8:
     case 10:
     case 12:
-        songay = 31;
+        songay_trongthang = 31;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        songay_trongthang = 30;
         break;
     case 2:
-        songay = 28 + ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0));
-        break;
-    default:
-        songay = 30;
-    }
-    ngay = ngay + 1;
-    if (ngay > songay)
-    {
-        ngay = 1;
-        thang += 1;
-        if (thang > 12)
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
         {
-            thang = 1;
-            nam += 1;
+            songay_trongthang = 29;
+            break;
+        }
+        else
+        {
+            songay_trongthang = 28;
+            break;
+        }
+    default:
+        break;
+    }
+    day += 1;
+    if (day > songay_trongthang)
+    {
+        day = 1;
+        month += 1;
+        if (month > 12)
+        {
+            month = 1;
+            year += 1;
         }
     }
-    printf("%d/%d/%d", ngay, thang, nam);
+    printf("Sau 1 ngay vua nhap: %d/%d/%d", day, month, year);
+
     return 0;
 }
