91ab086a4a3ae43bcdeed913e126e2f4ad1fead2
DungNguyenManh <dung903698954@gmail.com>
2021-12-18 14:19:22 +0700

    Try

--- Nháp/New folder/Bai24.cpp
+++ Nháp/New folder/Bai24.cpp
@@ -2,19 +2,17 @@
 
 int main()
 {
-    int n, T = 1;
+    int n, count = 0;
     printf("Nhap vao n: ");
     scanf("%d", &n);
-    printf("Cac uoc so cua %d la: ", n);
+    printf("Cac uoc so le cua %d la: ", n);
     for (int i = 1; i < n; i++)
     {
-        if (n % i == 0)
+        if (n % i == 0 && i % 2 == 1)
         {
-            T *= i;
             printf("%d ", i);
         }
     }
-    printf("\nTich cac uoc so cua %d la: %d", n, T);
 
     return 0;
 }
