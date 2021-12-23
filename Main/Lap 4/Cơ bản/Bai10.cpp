82d403c045682bcb6215180a17170210b3e892b4
DungNguyenManh <dung903698954@gmail.com>
2021-12-23 21:11:57 +0700

    Change

--- Main/Lap 4/Cơ bản/Bai10.cpp
+++ Main/Lap 4/Cơ bản/Bai10.cpp
@@ -1,16 +1,1 @@
-#include <stdio.h>
-
-int main()
-{
-    int n, sum = 0, i = 0;
-    while (n > 0)
-    {
-        i++;
-        printf("Nhap vao so nguyen %d: ",i);
-        scanf("%d",&n);
-        sum += n;
-    }
-    printf("%d",sum);
-
-    return 0;
-}   
+c
