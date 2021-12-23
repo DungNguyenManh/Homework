a63fd0aea97b29d6302b1feb5215dec0efc49c9b
DungNguyenManh <dung903698954@gmail.com>
2021-12-23 21:01:42 +0700

    Add

--- Main/Lap 4/Cơ bản/Bai4.cpp
+++ Main/Lap 4/Cơ bản/Bai4.cpp
@@ -1,34 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int Nhap_So(char c);
-void Kiem_Tra(int n);
-void Tong_So_Nguyen(int n);
-
-int main()
+int main ()
 {
-    int n;
-    n = Nhap_So('n');
-    printf("Tong cac so <= %d va thoa 1 trong 2 dk la: ", n);
-    Tong_So_Nguyen(n);
-}
-
-int Nhap_So(char c)
-{
-    int n;
-    printf("Nhap so nguyen duong %c: ", c);
-    scanf("%d", &n);
-    return n;
-}
-
-void Tong_So_Nguyen(int n)
-{
-    int sum = 0;
-    for (int i = 0; i <= n; i++)
-    {
-        if (n >= i && ((i % 3 == 0 && i % 5 == 0) || (i % 3 == 2 && i % 5 == 3)))
-        {
-            sum += i;
-        }
+    for(int i = 10; i >= 1; i--){
+        printf("%d\n",i);
     }
-    printf("%d", sum);
+    printf("Happy New Year");
+
+    return 0;
 }
