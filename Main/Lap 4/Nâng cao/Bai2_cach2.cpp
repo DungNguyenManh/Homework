16136446805739965353ad7255a08af9ae74c80f
DungNguyenManh <dung903698954@gmail.com>
2021-12-24 10:04:35 +0700

    Change

---
+++ Main/Lap 4/Nâng cao/Bai2_cach2.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0, max = 0;
+    do
+    {
+        printf("Nhap n: ");
+        scanf("%d", &n);
+        if (n < 0)
+        {
+            printf("Nhap lai\n");
+        }
+    } while (n < 0);
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+        if (sum <= n)
+        {
+            if (i == 1)
+            {
+                printf("%d ", i);
+            }
+            else
+            {
+                printf("+ %d ", i);
+            }
+        }
+    }
+    printf("<= %d", n);
+
+    return 0;
+}
