1c95a52e355ac13a8a2a40c3f9d31cc6361bb79c
DungNguyenManh <dung903698954@gmail.com>
2021-12-28 08:50:29 +0700

    Change

--- Nháp/New folder/Nhap.cpp
+++ Nháp/New folder/Nhap.cpp
@@ -1,35 +1,81 @@
 #include <stdio.h>
-#include <math.h>
-#define Max 100
-
-void Liet_Ke_So_Chinh_Phuong(int n);
-
+#include <ostream>
+void menu();
+int nhapso1(int a);
+int nhapso2(int b);
+void UCLN(int a, int b);
 int main()
 {
-    int n, a[Max];
-    printf("Nhap n: ");
-    scanf("%d", &n);
-    Liet_Ke_So_Chinh_Phuong(n);
+    int a, b;
+    menu();
+    a = nhapso1(a);
+    b = nhapso2(b);
+    return 0;
 }
 
-bool Kiem_Tra_So_Chinh_Phuong(int n)
+void menu()
 {
-    if (sqrt(n) * sqrt(n) == n)
+    int a = 0, b = 0;
+    bool tieptuc = true;
+    char chucnang;
+    while (tieptuc)
     {
-        return true;
+        printf("\n\n CHUONG TRINH CO CAC CHUC NANG SAU:");
+        printf("\na.-Cho nhap so thu nhat (a).");
+        printf("\nb.-Cho nhap so thu hai (b).");
+        printf("\nc.-tim uoc so chung lon nhat cua a va b.");
+        printf("\nd.-cho biet tong cac so hang trong tung so a va b, tong nao lon hon.");
+        printf("\ne.-in cac bang cuu chung tu a den b (hoac b den a).");
+        printf("\n Ban chon (a-e): ");
+        scanf("%c", &chucnang);
+        switch ('chucnang')
+        {
+        case 'a':
+            a = nhapso1(a);
+            break;
+        case 'b':
+            b = nhapso2(b);
+            break;
+        case 'c':
+            UCLN(a, b);
+            break;
+        case 'd':
+            printf("-----Good evening!-----");
+            break;
+        default:
+            printf("-----Good evening!-----");
+            
     }
-    return false;
 }
 
-void Liet_Ke_So_Chinh_Phuong(int n) 
+int nhapso1()
 {
-    printf("Tong cac so chinh phuong nho hon %d la: ",n);
-    int sum = 0;
-    for (int i = 4; i < n; i++)
+    int a
+    printf("nhap so a:");
+    scanf("%d", &a);
+    return a;
+}
+
+int nhapso2(int b)
+{
+    printf("nhap so b:");
+    scanf("%d", &b);
+    return b;
+void UCLN(int a, int b)
+{
+    if (a == 0 && b == 0)
     {
-        if(Kiem_Tra_So_Chinh_Phuong(i) == true){
-            sum += i;
-        }
-    }printf("%d",sum);
-        
+        a = nhapso1(a);
+        b = nhapso2(b);
+    }
+    int a1 = a;
+    int b1 = b;
+    while (a != b)
+    {
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+    printf("UCLN cua %d va %d la: %d", a1, b1, a);
 }
