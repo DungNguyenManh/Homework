9a82e41a8e20916c872914faac695b5248dc5021
DungNguyenManh <dung903698954@gmail.com>
2022-01-01 09:40:06 +0700

    Change

--- Nháp/New folder/Lap 2/Cơ bản/Bai5.cpp
+++ Nháp/New folder/Lap 2/Cơ bản/Bai5.cpp
@@ -1,29 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int Nhap_So(char c);
 void Tong_Hieu_Tich_Thuong(int x, int y);
 
-int main(){
+int main()
+{
     int x, y;
     x = Nhap_So('x');
     y = Nhap_So('y');
-    Tong_Hieu_Tich_Thuong(x,y);
+    Tong_Hieu_Tich_Thuong(x, y);
 
     return 0;
 }
 
-int Nhap_So(char c){
+int Nhap_So(char c)
+{
     int n;
     cout << "Nhap " << c << ": ";
     cin >> n;
     return n;
 }
 
-void Tong_Hieu_Tich_Thuong(int x, int y){
-    cout << "" << x << " + " << y << " = " << x+y;
-    cout << "\n" << x << " - " << y << " = " << x-y;
-    cout << "\n" << x << " * " << y << " = " << x*y;
-    cout << "\n" << x << " / " << y << " = " << x/y;
-
+void Tong_Hieu_Tich_Thuong(int x, int y)
+{
+    cout << "" << x << " + " << y << " = " << x + y;
+    cout << "\n"
+         << x << " - " << y << " = " << x - y;
+    cout << "\n"
+         << x << " * " << y << " = " << x * y;
+    cout << "\n"
+         << x << " / " << y << " = " << (float)x / y;
 }
