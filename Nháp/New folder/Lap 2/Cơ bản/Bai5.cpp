52793858f984d8e322218d31ed12b334d74226a6
DungNguyenManh <dung903698954@gmail.com>
2022-01-01 09:38:04 +0700

    Change

--- Nháp/New folder/Lap 2/Cơ bản/Bai5.cpp
+++ Nháp/New folder/Lap 2/Cơ bản/Bai5.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int Nhap_So(char c);
+void Tong_Hieu_Tich_Thuong(int x, int y);
 
 int main(){
     int x, y;
     x = Nhap_So('x');
     y = Nhap_So('y');
+    Tong_Hieu_Tich_Thuong(x,y);
 
     return 0;
 }
