ebe2693108dff3d13ba10c39cefef32fa189cbc1
DungNguyenManh <dung903698954@gmail.com>
2022-01-02 11:23:26 +0700

    Change

--- Main/Lap 6/Cơ bản/Bai1.cpp
+++ Main/Lap 6/Cơ bản/Bai1.cpp
@@ -21,7 +21,7 @@ int Nhap_So(int n)
 void Uoc_So(int n)
 {
     printf("Cac uoc so cua %d la: ", n);
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         if (n % i == 0)
         {
