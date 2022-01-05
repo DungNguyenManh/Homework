eedfcd9fdb3bedc185964dea8ea140592944466c
DungNguyenManh <dung903698954@gmail.com>
2022-01-05 11:30:08 +0700

    Change

---
+++ Main/Lap 6/Cơ bản/Bai10.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int Nhap_So(int n);
+
+int main (){
+    int n = Nhap_So(n);
+
+
+
+    return 0;
+}
+
+int Nhap_So(int n)
+{
+    printf("Nhap so nguyen duong n: ");
+    scanf("%d",&n);
+
+    return n;
+}
+
+void Cach_Doc(int n)
+{
+    int temp = n;   
+    
+    
+
+
+}
