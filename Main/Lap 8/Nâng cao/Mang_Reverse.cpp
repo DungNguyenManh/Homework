f7273d832d7c70489bfcec5ffafc583bb11436b8
DungNguyenManh <dung903698954@gmail.com>
2022-01-12 13:14:26 +0700

    Change

--- Main/Lap 8/Nâng cao/Mang_Reverse.cpp
+++ Main/Lap 8/Nâng cao/Mang_Reverse.cpp
@@ -6,6 +6,8 @@ void Nhap_Kich_Thuoc_Mang(int &n);
 void Nhap_Mang(int a[], int n);
 void Xuat_Mang(int a[], int n);
 void In_Mang_Dao_Nguoc(int a[], int n);
+void Dao_Nguoc_Mang(int a[], int n);
+void Mang_Dao_Nguoc(int a[], int n);
 
 int main()
 {
@@ -14,7 +16,8 @@ int main()
     Nhap_Mang(a, n);
     Xuat_Mang(a, n);
     In_Mang_Dao_Nguoc(a,n);
-
+    Dao_Nguoc_Mang(a,n);
+    Mang_Dao_Nguoc(a,n);
 
     return 0;
 }
@@ -45,7 +48,7 @@ void Xuat_Mang(int a[], int n)
 
 void In_Mang_Dao_Nguoc(int a[], int n)
 {
-    printf("\nIn ra mang dao nguoc: ");
+    printf("\nIn nguoc mang: ");
     for(int i = n-1; i >= 0; i--){
         printf("%2d ",a[i]);
     }
@@ -54,6 +57,16 @@ void In_Mang_Dao_Nguoc(int a[], int n)
 void Dao_Nguoc_Mang(int a[], int n)
 {
     for(int i = 0; i < n/2; i++){
-        
+        int temp = a[i];
+        a[i] = a[n-i-1];
+        a[n-i-1] = temp;
+    }
+}
+
+void Mang_Dao_Nguoc(int a[], int n)
+{
+    printf("\nMang dao nguoc la: ");
+    for(int i = 0; i < n; i++){
+        printf("%2d ",a[i]);
     }
 }
