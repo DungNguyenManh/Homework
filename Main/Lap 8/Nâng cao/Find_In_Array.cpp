9985c4867512efc989ed752ce758cf1590606ccf
DungNguyenManh <dung903698954@gmail.com>
2022-01-12 13:15:56 +0700

    Change

---
+++ Main/Lap 8/Nâng cao/Find_In_Array.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define Max 100
+
+void Nhap_Kich_Thuoc_Mang(int &n);
+void Nhap_Mang(int a[], int n);
+void Xuat_Mang(int a[], int n);
+void In_Mang_Dao_Nguoc(int a[], int n);
+void Dao_Nguoc_Mang(int a[], int n);
+void Mang_Dao_Nguoc(int a[], int n);
+
+int main()
+{
+    int a[Max], n;
+    Nhap_Kich_Thuoc_Mang(n);
+    Nhap_Mang(a, n);
+    Xuat_Mang(a, n);
+    In_Mang_Dao_Nguoc(a,n);
+    Dao_Nguoc_Mang(a,n);
+    Mang_Dao_Nguoc(a,n);
+
+    return 0;
+}
+
+void Nhap_Kich_Thuoc_Mang(int &n)
+{
+    printf("Nhap kich thuoc mang: ");
+    scanf("%d", &n);
+}
+
+void Nhap_Mang(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("Nhap a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+}
+
+void Xuat_Mang(int a[], int n)
+{
+    printf("Mang vua nhap la: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%2d ", a[i]);
+    }
+}
