63787a599961107f653639e77c5e87dd784e09aa
your name <dung903698954@gmail.com>
2022-08-02 07:25:00 +0700

    add

--- Main/Lap 8/Nâng cao/Bai5.cpp
+++ Main/Lap 8/Nâng cao/Bai5.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 
-void Hinh6(int n);
+void Hinh8(int n);
 
 int main()
 {
     int n;
     printf("Nhap n: ");
     scanf("%d", &n);
-    Hinh6(n);
+    Hinh8(n);
 
     return 0;
 }
@@ -56,7 +56,7 @@ void Hinh3(int n)
     {
         for (int j = 1; j <= 2 * n - 1; j++)
         {
-            if(i + j == n + 1 || i == n || j == n + i - 1)
+            if (i + j == n + 1 || i == n || j == n + i - 1)
                 printf(" * ");
             else
                 printf("   ");
@@ -74,7 +74,7 @@ void Hinh4(int n)
     {
         for (int j = 1; j <= 2 * n - 1; j++)
         {
-            if(i == 1 || i == j || i + j == 2 * n)
+            if (i == 1 || i == j || i + j == 2 * n)
                 printf(" * ");
             else
                 printf("   ");
@@ -92,7 +92,7 @@ void Hinh5(int n)
     {
         for (int j = 1; j <= n; j++)
         {
-            if(j == n || i == j || i + j == 2 * n)
+            if (j == n || i == j || i + j == 2 * n)
                 printf("* ");
             else
                 printf("  ");
@@ -108,16 +108,48 @@ void Hinh6(int n)
     printf("\nHinh 6\n");
     for (int i = 1; i <= 2 * n - 1; i++)
     {
-        for(int k = 1; k <= abs(n - i); k++)
+        for (int j = 1; j <= n; j++)
+        {
+            if (j == n || j + i == n + 1 || j == abs(n - i) + 1)
+                printf("* ");
+            else
+                printf("  ");
+        }
+
+        printf("\n");
+    }
+    printf("\n\n");
+}
+
+void Hinh7(int n)
+{
+    printf("\nHinh 7\n");
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i >= j && i + j <= 2 * n)
+                printf("* ");
+        }
+
+        printf("\n");
+    }
+    printf("\n\n");
+}
+
+void Hinh8(int n)
+{
+    printf("\nHinh 8\n");
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        for (int k = 1; k <= abs(n - i); k++)
         {
             printf("  ");
         }
         for (int j = 1; j <= n; j++)
         {
-            if(j == 1 || j == i)
+            if (j >= abs(n - i) + 1)
                 printf("* ");
-            else
-                printf("  ");
         }
 
         printf("\n");
